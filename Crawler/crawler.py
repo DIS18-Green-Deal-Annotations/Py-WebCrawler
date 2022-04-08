3ad7399890113b7e2707b6de155839ba7af1d4c1
Gilles Romer <gilles_alain_luc.romer@smail.th-koeln.de>
2022-04-08 16:52:51 +0200

    renamed

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -1,11 +1,15 @@
 from bs4 import BeautifulSoup
 import get_links
 import get_tables
+import check_new
 linklist = get_links.fromURL(r"https://ec.europa.eu/info/strategy/priorities-2019-2024/european-green-deal/delivering-european-green-deal_en#documents")
+
 
 # print(len(linklist.get())) # Printing number of links
 # print(*linklist.get(), sep="\n") # Printing all links to the documents
 
-for link in linklist.get():
-    tables = get_tables.fromURL(link)
-    print(tables.get())
+# for link in linklist.get():
+#     tables = get_tables.fromURL(link)
+#     print(tables.get())
+
+check_new.check()
