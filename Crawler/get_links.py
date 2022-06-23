dd868d78f8f07abf624ff702d6f6ef5ce3560a77
Gilles Romer <gilles_alain_luc.romer@smail.th-koeln.de>
2022-06-23 14:13:25 +0200

    downloads de version if en not available

--- Crawler/get_links.py
+++ Crawler/get_links.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 class fromURL:
     def __init__(self, url):
         self.url = url
@@ -10,12 +11,16 @@ class fromURL:
         document_links = []
         for documents in document_list.find_all("div", class_ = "field field-name-field-core-legacy-link field--field-core-legacy-link"):
             link = documents.find("div", class_ = "file").a["href"]
-            link = link.replace("nl/", "en/").replace("%3A", ":")
-            link = link.replace("ga/", "de/").replace("%3A", ":")
-            # Check if link contains qid
+            content_page_link = BeautifulSoup(requests.get(link).content, "lxml")
+            link = re.sub("nl/", "en/", link, flags=re.IGNORECASE).replace("%3A", ":")
+            if content_page_link.find(id="format_language_table_HTML_EN", class_ = "disabled"):
+                link = re.sub("en/", "de/", link, flags=re.IGNORECASE)
+                # Check if link contains qid
+            elif content_page_link.find(id="format_language_table_HTML_DE", class_ = "disabled"):
+                print("Skipped document with following link as no known language could be found!\n", link)
             if link.rfind("qid=") != -1:
                 # Removing non-necessary qid part
-                link = link[:link.rfind("qid=") -1] 
+                link = link[:link.rfind("qid=") -1]
             html = link.rfind("TXT/")
             link = link[:html + 4] + "HTML/?" + link[html + 5:]
             document_links.append(link)
