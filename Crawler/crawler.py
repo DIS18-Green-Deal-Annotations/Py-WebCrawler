0ee355d1503ce328057adb3f91c59022992baa6c
Gilles Romer <gilles_alain_luc.romer@smail.th-koeln.de>
2022-06-23 15:09:08 +0200

    now runs the check and asks for table download

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -1,15 +1,27 @@
-from bs4 import BeautifulSoup
 import get_links
 import get_tables
 import check_new
 linklist = get_links.fromURL(r"https://ec.europa.eu/info/strategy/priorities-2019-2024/european-green-deal/delivering-european-green-deal_en#documents")
 
-
-# print(len(linklist.get())) # Printing number of links
-# print(*linklist.get(), sep="\n") # Printing all links to the documents
-
-# for link in linklist.get():
-#     tables = get_tables.fromURL(link)
-#     print(tables.get())
-
-check_new.check()
+done = False
+while done is False:
+    try:
+        if __name__ == "__main__":
+            print("Checking for new documents ...")
+            check_new.check()
+            print("Done!")
+        
+        download_tables = input("Would you like to download the tables of each document to './html_tables' ? [y/n]: ").lower()
+        if download_tables == "y":
+            count = 0
+            for link in linklist.get():
+                count += 1
+                tables = get_tables.fromURL(link)
+                tables.download()
+                print(f"Downloaded tables from {count} document" + ("s" if count > 1 or count == 0 else ""), end="\r")
+            print("\n")
+        
+        done = True
+    except AttributeError:
+        print("Error. Trying again ...")
+        pass
